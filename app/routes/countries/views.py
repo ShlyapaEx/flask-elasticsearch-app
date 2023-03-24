21b0c22a1aa150c28542ce2378bc155f1861e088
ShlyapaEx <musha742@gmail.com>
2023-03-24 12:59:06 +0300

    jinja templates init

--- app/routes/countries/views.py
+++ app/routes/countries/views.py
@@ -1,17 +1,34 @@
-from flask import Blueprint, Response, make_response
+from flask import Blueprint, Response, render_template, request, flash
+
+from app.ext.sqlalchemy import db
 
 from .models import Country
 from .queries import get_all_countries_query
 from .schemas import CountrySchema
 
 countries: Blueprint = Blueprint(name='countries',
-                                 import_name=__name__)
+                                 import_name=__name__,
+                                 template_folder='templates')
 
 
 @countries.route('/countries', methods=['get'])
 def get_all_countries():
     all_countries = get_all_countries_query()
     return CountrySchema(many=True).dump(all_countries)
+
+
+@countries.route('/countries/create', methods=['get', 'post'])
+def add_new_country():
+    if request.method == 'POST':
+        name = request.form['name']
+        if not name:
+            flash('Name is required!', 'error')
+        else:
+            new_country = Country(name=name)  # type: ignore
+            db.session.add(new_country)
+            db.session.commit()
+            flash('Success!', 'message')
+    return render_template('coffee_creation.jinja2')
 
 
 @countries.route('/countries/reindex', methods=['get'])
